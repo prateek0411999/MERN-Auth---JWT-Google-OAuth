b2451f695f0f1954248eb9098c631323b90bc4cd
Prateek Sharma <prateekshrmprateek@gmail.com>
2021-02-09 06:07:20 +0530

    deployed serverSide- heroku, clientSide- Netlify

--- server/index.js
+++ server/index.js
@@ -14,9 +14,11 @@ app.use(bodyParser.urlencoded({limit: '30mb', extended: true}))
 app.use(cors());
 
 app.use('/posts',postRoutes)
+app.use('/',(req,res)=>{
+    res.send('HEllo to memories API')
+});
 
-
-const PORT= process.env.PORT || 5000
+const PORT= process.env.PORT || 5000;
 
 mongoose.connect(process.env.Connection_URL, { useNewUrlParser: true, useUnifiedTopology: true})
     .then(() =>{
